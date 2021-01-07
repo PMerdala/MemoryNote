b0904872201501523891fc5af8af68bba1ad012d
PMerdala <P.Merdala@gmail.com>
2021-01-07 15:50:34 +0100

    add NoteEntity

---
+++ app/src/main/java/pl/merdala/memorynote/framework/db/NoteEntity.kt
@@ -0,0 +1,26 @@
+package pl.merdala.memorynote.framework.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import pl.merdala.core.data.Note
+
+@Entity
+data class NoteEntity(
+    val title: String,
+    val content: String,
+    @ColumnInfo(name = "creation_date")
+    val creationDate: Long,
+    @ColumnInfo(name = "update_date")
+    val updateDate: Long,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L
+) {
+    companion object {
+        fun fromNote(note: Note) =
+            NoteEntity(note.title, note.content, note.creationDate, note.updateDate)
+    }
+
+    fun toNote() = Note(title, content, creationDate, updateDate, id)
+
+}
