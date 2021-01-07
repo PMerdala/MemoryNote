7ff74096eb9b571f7e2a1a3c9e5a037079bd7620
PMerdala <P.Merdala@gmail.com>
2021-01-07 01:26:01 +0100

    Add MainActivity

---
+++ app/src/main/java/pl/merdala/memorynote/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.merdala.memorynote.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.merdala.memorynote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
